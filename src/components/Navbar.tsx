edf2a2a7eff8ee249af5a74a80104249e4b87d86
_Ero_ <60323827+7xErox7@users.noreply.github.com>
2023-10-16 08:43:35 -0300

    feat *

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { User } from "../utils/types";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
+import { FaBars } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md';
 import Image from "next/image";
 import DefaultAvatar from '../styles/assets/default-avatar.png';
 
@@ -12,17 +14,13 @@ export default function Navbar(){
     const {data,status} = useSession();
     const user = data?.user as User | undefined
     const router = useRouter();
-    function getStyle(id:string):typeof items_styles[0] | undefined{
-        const style = items_styles.find((a:any)=>a.id === id);
-        return style ? style : undefined;
-    }
     return (
         <header className="navbar-header">
             <a href="/" className="Logo">Suporte Eletro</a>
             <input type="checkbox" id="check"/>
             <label htmlFor="check" className="icons">
-                <i className='bx bx-menu'id="menu-icon"></i>
-                <i className='bx bx-x'id="close-icon"></i>
+                <FaBars className={navbar ? 'bx bx-x' : 'bx bx-menu'} id="menu-icon" onClick={e=>setNavbar(!navbar)}/>
+                <MdClose className={navbar ? 'bx bx-menu' : 'bx bx-x'} id="close-icon" onClick={e=>setNavbar(!navbar)}/>
             </label>
             <nav className="navbar">
             <a  href="/">Home</a>
