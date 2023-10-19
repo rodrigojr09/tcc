360f680c8b8d4800ee92a27cc63391d6a48e1812
_Ero_ <60323827+7xErox7@users.noreply.github.com>
2023-10-19 12:13:24 -0300

    feat *

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,8 +14,136 @@ export default function Navbar(){
     const {data,status} = useSession();
     const user = data?.user as User | undefined
     const router = useRouter();
-    return (
-        <header className="navbar-header">
+    return ( <>
+        <style jsx>
+            { `.navbar-header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    padding: 1.3rem 10%;
+   /* background:rgba(0, 0, 0, .1);*/
+    /*backdrop-filter:blur(50px) ;*/
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 100;
+}
+.navbar-header::before{
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100%;
+    background: rgba(0, 0, 0, .1);
+    backdrop-filter: blur(50px);
+    z-index: -1;
+}
+
+.navbar-header .Logo{
+    font-size: 2rem;
+    color: #DB0000;
+    text-decoration: none;
+    font-weight: 700;
+}
+.navbar-header .navbar a {
+    position: relative;
+    font-size: 1.15rem;
+    color: #fff;
+    text-decoration: none;
+    font-weight: 500;
+    margin-left: 2.7rem;
+}
+
+.navbar-header .navbar a::before{
+    content: '';
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 0;
+    height: 1px;
+    background: #fff;
+    transition: .3s;
+}
+
+.navbar-header .navbar a:hover::before{
+    width: 100%;
+}
+
+.navbar-header #check {
+    display: none;
+}
+.navbar-header .icons{
+    position: absolute;
+    right: 5%;
+   font-size:2.8rem;
+   color: #fff;
+   cursor: pointer; 
+   display: none; 
+}
+@media only screen and (max-width:600px){
+    .navbar-header::before {
+        width: 100%;
+    }
+    .navbar-header {
+        width: 100%;
+    }
+    .navbar-header .icons {
+        display: inline-flex;
+    }
+    
+    .navbar-header #check:checked~.icons #menu-icon{
+        display: none;  
+    }
+    .navbar-header .icons #close-icon{
+        display:none;
+    }
+    .navbar-header #check:checked~.icons #close-icon{
+        display: block;
+    }
+    .navbar-header .navbar{
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        height: 0;
+        background:rgba(0, 0, 0, .1);
+        backdrop-filter: blur(50px);
+        box-shadow: 0.5rem 1rem rgba(0, 0, 0, .1);
+        overflow: hidden;
+        transition: .3s ease;
+    }
+    .navbar-header #check:checked~.navbar{
+        height: 8rem;
+    }
+    .navbar-header .navbar a {
+        display: block;
+        font-size: 1.1rem;
+        margin: 0.8rem 0;  
+        text-align: center; 
+        transform:translateY(-50);
+        transition:.3s ease ;
+    }
+    .navbar-header #check:checked~.navbar a { 
+        transform: translateY(0);
+        opacity: 1;
+        transition-delay: calc(.15s * var(--i));
+    }
+    .navbar-header .bx-menu {
+        font-size: 4vh;
+        color: white;
+    }
+    
+}    
+/*.navbar-header .Logo */
+@media (max-width:992px){
+    .navbar-header .header{
+        padding:1.3rem 5%;
+    }
+}`}
+        </style>
+        <header  className={"navbar-header"}>
             <a href="/" className="Logo">Suporte Eletro</a>
             <input type="checkbox" id="check"/>
             <label htmlFor="check" className="icons">
@@ -28,5 +156,6 @@ export default function Navbar(){
             <a  href={user ? "/aluno" : "/auth/login"}>{user ? (user?.nome.split(" ")[0].split("").map((a,i)=>i === 0 ? a : a.toLowerCase())) : "Login"}</a>    
             </nav>
         </header>
+    </>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,73 @@ import Navbar from '../components/Navbar'
 import { User } from '../utils/types';
 import HomeImage from '../styles/assets/home-img.svg';
 import Image from 'next/image';
+//import '../styles/home.css';
 
 export default function Index() {
   const {data,status} = useSession();
   const user = data?.user as User | undefined; 
-  return (
+  return (<>
+    <style jsx>
+ {`
+    .main-home .left-home{
+        width: 50vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .main-home .left-home > h1{
+        color: #fff;
+        font-size:22pt;
+    }
+
+    .main-home .left-home-image{
+        width: 35vw;
+    }
+
+    .main-home .right-home{
+        width: 43vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-left: 20px;
+        padding-right: 20px;
+        flex-direction: column;
+    }
+
+    .main-home .right-home > h1{
+        font-size:25pt;
+        color: #fff;
+    }
+
+    .main-home .btn-entrar{
+        height: 50px;
+        width: 220px;
+        padding: 12px 0px;
+        margin:25px;
+        border: none;
+        border-radius: 8px;
+        outline: none;
+        text-transform: uppercase;
+        font-weight: 800;
+        letter-spacing: 3px;
+        color:#fff;
+        display: flex;
+        justify-items: center;
+        justify-content: center;
+        background: #DB0000;
+        cursor: pointer;
+        box-shadow: 0px 10px 40px -12px #db00004e;
+        transition: transform 200ms ease-in-out;
+    }
+
+    .main-home .btn-entrar:hover{
+        transform: scale(1.1);
+    }
+    `} 
+    </style>
     <div className="main-home">
       <div className="left-home">
         <Image src={HomeImage} width={0} height={0} className="left-home-image"  alt="home_image.svg" />
@@ -24,5 +86,6 @@ export default function Index() {
         <a href={user ? "/aluno" : "/auth/login"} className="btn-entrar">Entrar</a>
       </div>
     </div>
+    </>
   )
 }
